3c7fbf9494336a5a282143e17b914b84da9d8522
Arnold Daniels <arnold@jasny.net>
2016-08-23 22:34:32 +0200

    Added abstract methods to Uri/Authority trait

--- src/Uri/Authority.php
+++ src/Uri/Authority.php
@@ -8,6 +8,31 @@ namespace Jasny\HttpMessage\Uri;
 trait Authority
 {
     /**
+     * Retrieve the user information component of the URI.
+     *
+     * @return string The URI user information, in "username[:password]" format.
+     */
+    abstract public function getUserInfo();
+    
+    /**
+     * Retrieve the host component of the URI.
+     *
+     * @return string The URI host.
+     */
+    abstract public function getHost();
+    
+    /**
+     * Retrieve the port component of the URI.
+     *
+     * If a port is present, and it is non-standard for the current scheme,
+     * this method will return it as an integer.
+     *
+     * @return null|int The URI port.
+     */
+    abstract public function getPort();
+    
+    
+    /**
      * Retrieve the authority component of the URI.
      *
      * The authority syntax of the URI is:
