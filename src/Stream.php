ea6cbeb38bbbe024daf0ea3daa6227a81ce164d5
Arnold Daniels <arnold@jasny.net>
2016-08-30 05:01:45 +0200

    Added Stream implementation

---
+++ src/Stream.php
@@ -0,0 +1,296 @@
+<?php
+
+namespace Jasny\HttpMessage;
+
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * An instance wraps a PHP stream and can be used for a PSR-7 implementation.
+ * This interface provides a wrapper around the most common operations, including
+ * serialization of the entire stream to a string.
+ */
+class Stream implements StreamInterface
+{
+    /**
+     * @var resource 
+     */
+    protected $handle;
+    
+    /**
+     * Class constructor
+     * 
+     * @param resource $handle
+     */
+    public function __construct($handle)
+    {
+        if (!is_resource($handle) || get_resource_type($handle) !== 'stream') {
+            throw new \InvalidArgumentException('Argument must be a PHP stream resource');
+        }
+        
+        $this->handle = $handle;
+    }
+    
+    
+    /**
+     * Reads all data from the stream into a string, from the beginning to end.
+     *
+     * Warning: This could attempt to load a large amount of data into memory.
+     *
+     * @see http://php.net/manual/en/language.oop5.magic.php#object.tostring
+     * @return string
+     */
+    public function __toString()
+    {
+        try {
+            if ($this->isSeekable()) {
+                $this->rewind();
+            }
+            
+            return $this->getContents();
+        } catch (\Exception $e) {
+            return '';
+        }
+    }
+
+    /**
+     * Closes the stream and any underlying resources.
+     */
+    public function close()
+    {
+        if (!$this->isClosed()) {
+            fclose($this->handle);
+        }
+    }
+
+    /**
+     * Check if the stream is closed
+     * 
+     * @return boolean
+     */
+    protected function isClosed()
+    {
+        return !isset($this->handle) || get_resource_type($this->handle) !== 'stream';
+    }
+    
+    /**
+     * Separates any underlying resources from the stream.
+     *
+     * After the stream has been detached, the stream is in an unusable state.
+     *
+     * @return resource|null Underlying PHP stream, if any
+     */
+    public function detach()
+    {
+        $handle = $this->handle;
+        $this->handle = null;
+        
+        return $handle;
+    }
+
+    /**
+     * Get the size of the stream if known.
+     *
+     * @return int|null Returns the size in bytes if known, or null if unknown.
+     */
+    public function getSize()
+    {
+        if (!$this->isClosed()) {
+            $stat = fstat($this->handle);
+        }
+        
+        return isset($stat['size']) ? $stat['size'] : null;
+    }
+
+    /**
+     * Returns the current position of the file read/write handle
+     *
+     * @return int Position of the file handle
+     * @throws \RuntimeException on error.
+     */
+    public function tell()
+    {
+        if (!$this->isClosed()) {
+            $pos = ftell($this->handle);
+        }
+        
+        if (!isset($pos) || $pos === false) {
+            throw new \RuntimeException("Failed to get the position of the stream");
+        }
+        
+        return $pos;
+    }
+
+    /**
+     * Returns true if the stream is at the end of the stream.
+     *
+     * @return bool
+     */
+    public function eof()
+    {
+        return $this->isClosed() || feof($this->handle);
+    }
+
+    /**
+     * Returns whether or not the stream is seekable.
+     *
+     * @return bool
+     */
+    public function isSeekable()
+    {
+        return $this->getMetadata('seekable') ?: false;
+    }
+
+    /**
+     * Seek to a position in the stream.
+     *
+     * @see http://www.php.net/manual/en/function.fseek.php
+     * @param int $offset Stream offset
+     * @param int $whence Specifies how the cursor position will be calculated
+     *     based on the seek offset. Valid values are identical to the built-in
+     *     PHP $whence values for `fseek()`.  SEEK_SET: Set position equal to
+     *     offset bytes SEEK_CUR: Set position to current location plus offset
+     *     SEEK_END: Set position to end-of-stream plus offset.
+     * @throws \RuntimeException on failure.
+     */
+    public function seek($offset, $whence = SEEK_SET)
+    {
+        if (!$this->isSeekable()) {
+            throw new \RuntimeException("Stream isn't seekable");
+        }
+        
+        $ret = fseek($this->handle, $offset, $whence);
+        
+        if ($ret === -1) {
+            throw new \RuntimeException("Failed to get the seek to postion in the stream");
+        }
+    }
+
+    /**
+     * Seek to the beginning of the stream.
+     *
+     * If the stream is not seekable, this method will raise an exception;
+     * otherwise, it will perform a seek(0).
+     *
+     * @see seek()
+     * @see http://www.php.net/manual/en/function.fseek.php
+     * @throws \RuntimeException on failure.
+     */
+    public function rewind()
+    {
+        $this->seek(0, SEEK_SET);
+    }
+
+    /**
+     * Returns whether or not the stream is writable.
+     *
+     * @return bool
+     */
+    public function isWritable()
+    {
+        $mode = $this->getMetadata('mode');
+        return isset($mode) && $mode[0] !== 'r';
+    }
+
+    /**
+     * Write data to the stream.
+     *
+     * @param string $string The string that is to be written.
+     * @return int Returns the number of bytes written to the stream.
+     * @throws \RuntimeException on failure.
+     */
+    public function write($string)
+    {
+        if (!$this->isWritable()) {
+            throw new \RuntimeException("Stream isn't writable");
+        }
+        
+        $ret = fwrite($this->handle, $string);
+        
+        if ($ret === false) {
+            throw new \RuntimeException("Failed to write to stream");
+        }
+        
+        return $ret;
+    }
+
+    /**
+     * Returns whether or not the stream is readable.
+     *
+     * @return bool
+     */
+    public function isReadable()
+    {
+        $mode = $this->getMetadata('mode');
+        return isset($mode) && ($mode[0] === 'r' || strpos($mode, '+') !== false);
+    }
+
+    /**
+     * Read data from the stream.
+     *
+     * @param int $length Read up to $length bytes from the object and return
+     *     them. Fewer than $length bytes may be returned if underlying stream
+     *     call returns fewer bytes.
+     * @return string Returns the data read from the stream, or an empty string
+     *     if no bytes are available.
+     * @throws \RuntimeException if an error occurs.
+     */
+    public function read($length)
+    {
+        if (!$this->isReadable()) {
+            throw new \RuntimeException("Stream isn't readable");
+        }
+        
+        $contents = fread($this->handle, $length);
+        
+        if ($contents === false) {
+            throw new \RuntimeException("Failed to read from stream");
+        }
+        
+        return $contents;
+    }
+
+    /**
+     * Returns the remaining contents in a string
+     *
+     * @return string
+     * @throws \RuntimeException if unable to read.
+     * @throws \RuntimeException if error occurs while reading.
+     */
+    public function getContents()
+    {
+        if (!$this->isReadable()) {
+            throw new \RuntimeException("Stream isn't readable");
+        }
+        
+        $contents = '';
+        
+        while (!$this->eof()) {
+            $contents .= $this->read(512000);
+        }
+        
+        return $contents;
+    }
+
+    /**
+     * Get stream metadata as an associative array or retrieve a specific key.
+     *
+     * The keys returned are identical to the keys returned from PHP's
+     * stream_get_meta_data() function.
+     *
+     * @see http://php.net/manual/en/function.stream-get-meta-data.php
+     * @param string $key Specific metadata to retrieve.
+     * @return array|mixed|null Returns an associative array if no key is
+     *     provided. Returns a specific key value if a key is provided and the
+     *     value is found, or null if the key is not found.
+     */
+    public function getMetadata($key = null)
+    {
+        $meta = !$this->isClosed() ? stream_get_meta_data($this->handle) : null;
+        
+        if (isset($key)) {
+            $meta = isset($meta[$key]) ? $meta[$key] : null;
+        }
+        
+        return $meta;
+    }
+}
---
+++ tests/StreamTest.php
@@ -0,0 +1,386 @@
+<?php
+
+namespace Jasny\HttpMessage\DerivedAttribute;
+
+use PHPUnit_Framework_TestCase;
+
+use Jasny\HttpMessage\Stream;
+
+/**
+ * @covers \Jasny\HttpMessage\Stream
+ */
+class StreamTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testConstructNonResource()
+    {
+        new Stream('foo');
+    }
+    
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testConstructNonStreamResource()
+    {
+        if (!function_exists('imagecreate')) {
+            return $this->markTestSkipped("GD library not loaded, which is used as non-stream resource");
+        }
+        
+        $resource = imagecreate(1, 1);
+        new Stream($resource);
+    }
+    
+    
+    
+    public function testClose()
+    {
+        $fp = fopen('data://text/plain,foo', 'r');
+        $stream = new Stream($fp);
+        
+        $stream->close();
+        
+        $this->assertEquals("Unknown", get_resource_type($fp));
+    }
+    
+    public function testDetach()
+    {
+        $fp = fopen('data://text/plain,foo', 'r');
+        $stream = new Stream($fp);
+        
+        $detached = $stream->detach();
+        $this->assertSame($fp, $detached);
+        
+        $this->assertEquals("", (string)$stream);
+        
+        $this->assertEquals("stream", get_resource_type($fp));
+        $this->assertEquals("foo", fread($fp, 1024));
+    }
+    
+    public function testGetSize()
+    {
+        $fp = fopen('data://text/plain,foo', 'r');
+        $stream = new Stream($fp);
+        
+        $this->assertEquals(3, $stream->getSize());
+    }
+    
+    
+    public function testIsSeekable()
+    {
+        $this->assertTrue((new Stream(fopen('php://memory', 'w+')))->isSeekable());
+        $this->assertTrue((new Stream(fopen('php://input', 'r')))->isSeekable());
+        $this->assertTrue((new Stream(fopen('data://text/plain,hello world', 'r')))->isSeekable());
+        
+        $this->assertFalse((new Stream(fopen('php://output', 'w')))->isSeekable());
+    }
+    
+    public function testIsReadable()
+    {
+        $this->assertTrue((new Stream(fopen('php://input', 'r')))->isReadable());
+        $this->assertTrue((new Stream(fopen('data://text/plain,hello world', 'r')))->isReadable());
+        $this->assertTrue((new Stream(fopen('php://memory', 'w+')))->isReadable());
+        
+        $this->assertFalse((new Stream(fopen('php://output', 'w')))->isReadable());
+    }
+    
+    public function testIsWritable()
+    {
+        $this->assertTrue((new Stream(fopen('php://memory', 'w+')))->isWritable());
+        $this->assertTrue((new Stream(fopen('php://output', 'w')))->isWritable());
+        
+        $this->assertFalse((new Stream(fopen('php://input', 'r')))->isWritable());
+        $this->assertFalse((new Stream(fopen('data://text/plain,hello world', 'r')))->isWritable());
+    }
+    
+    
+    public function testSeek()
+    {
+        $fp = fopen("data://text/plain,blue red yellow", 'r');
+        $stream = new Stream($fp);
+        
+        $stream->seek(5);
+        
+        $this->assertEquals("red yellow", $stream->read(1024));
+    }
+    
+    public function testSeekCur()
+    {
+        $fp = fopen("data://text/plain,blue red yellow", 'r');
+        $stream = new Stream($fp);
+        
+        $stream->seek(5, SEEK_CUR);
+        $this->assertEquals("red", $stream->read(3));
+        
+        $stream->seek(1, SEEK_CUR);
+        $this->assertEquals("yellow", $stream->read(6));
+        
+        $stream->seek(-3, SEEK_CUR);
+        $this->assertEquals("lo", $stream->read(2));
+    }
+    
+    public function testSeekEnd()
+    {
+        $fp = fopen("data://text/plain,blue red yellow", 'r');
+        $stream = new Stream($fp);
+        
+        $stream->seek(-6, SEEK_END);
+        $this->assertEquals("yellow", $stream->read(6));
+    }
+    
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Stream isn't seekable
+     */
+    public function testSeekNonSeekable()
+    {
+        $fp = fopen('php://output', 'w');
+        $stream = new Stream($fp);
+        
+        $stream->seek(5);
+    }
+    
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testSeekClosed()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        fclose($fp);
+        $stream->seek(5);
+    }
+
+    
+    public function testTell()
+    {
+        $fp = fopen("data://text/plain,blue red yellow", 'r');
+        $stream = new Stream($fp);
+        
+        $this->assertEquals(0, $stream->tell());
+        
+        $stream->read(4);
+        $this->assertEquals(4, $stream->tell());
+        
+        $stream->read(5);
+        $this->assertEquals(9, $stream->tell());
+        
+        $stream->read(100);
+        $this->assertEquals(15, $stream->tell());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testTellClosed()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        fclose($fp);
+        $stream->tell();
+    }
+    
+    
+    public function testEof()
+    {
+        $fp = fopen("data://text/plain,blue red yellow", 'r');
+        $stream = new Stream($fp);
+        
+        $this->assertFalse($stream->eof());
+        
+        $stream->read(4);
+        $this->assertFalse($stream->eof());
+        
+        $stream->read(100);
+        $this->assertFalse($stream->eof());
+        
+        $stream->read(100);
+        $this->assertTrue($stream->eof());
+    }
+
+    public function testEofClosed()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        fclose($fp);
+        $this->assertTrue($stream->eof());
+    }
+
+    
+    public function testWrite()
+    {
+        $fp = fopen('php://memory', 'w+');
+        $stream = new Stream($fp);
+        
+        $written = $stream->write("Foo bar");
+        $this->assertEquals(7, $written);
+        
+        fseek($fp, 0);
+        $contents = fread($fp, 1024);
+        $this->assertEquals("Foo bar", $contents);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Stream isn't writable
+     */
+    public function testWriteNonWritable()
+    {
+        $fp = fopen('data://text/plain,foo', 'r');
+        $stream = new Stream($fp);
+        
+        $stream->write("Foo bar");
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testWriteClosed()
+    {
+        $fp = fopen('php://memory', 'w+');
+        $stream = new Stream($fp);
+        
+        fclose($fp);
+        $stream->write("Foo bar");
+    }
+    
+    
+    public function testRead()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        $contents = $stream->read(1024);
+        $this->assertEquals("blue red yellow", $contents);
+    }
+    
+    public function testReadInParts()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        $this->assertEquals("blue", $stream->read(4));
+        $this->assertEquals(" red", $stream->read(4));
+        $this->assertEquals(" yel", $stream->read(4));
+        $this->assertEquals("low", $stream->read(4));
+    }
+    
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Stream isn't readable
+     */
+    public function testReadNonReadable()
+    {
+        $fp = fopen('php://output', 'w');
+        $stream = new Stream($fp);
+        
+        $stream->read(1024);
+    }
+    
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testReadClosed()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        fclose($fp);
+        $stream->read(1024);
+    }
+
+    
+    public function testGetContents()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        $this->assertEquals("blue red yellow", $stream->getContents());
+    }
+    
+    public function testGetContentsRest()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        $stream->read(5);
+        $this->assertEquals(5, $stream->tell());
+        
+        $this->assertEquals("red yellow", $stream->getContents());
+    }
+    
+    
+    public function testToString()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        $this->assertEquals("blue red yellow", (string)$stream);
+    }
+    
+    public function testToStringRewind()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        $stream->read(5);
+        $this->assertEquals(5, $stream->tell());
+        
+        $this->assertEquals("blue red yellow", (string)$stream);
+    }
+    
+    public function testToStringClosed()
+    {
+        $fp = fopen('data://text/plain,blue red yellow', 'r');
+        $stream = new Stream($fp);
+        
+        fclose($fp);
+        $this->assertEquals('', (string)$stream);
+    }
+    
+    
+    public function testGetMetadata()
+    {
+        $fp = fopen('data://text/plain,foo', 'r');
+        $stream = new Stream($fp);
+        
+        $expect = [
+            "mediatype" => "text/plain",
+            "base64" => false,
+            "wrapper_type" => "RFC2397",
+            "stream_type" => "RFC2397",
+            "mode" => "r",
+            "unread_bytes" => 0,
+            "seekable" => true,
+            "uri" => "data://text/plain,foo"
+        ];
+        
+        $this->assertEquals($expect, $stream->getMetadata());
+    }
+    
+    public function testGetMetadataByKey()
+    {
+        $fp = fopen('data://text/plain,foo', 'r');
+        $stream = new Stream($fp);
+        
+        $this->assertEquals("text/plain", $stream->getMetadata("mediatype"));
+        $this->assertEquals("RFC2397", $stream->getMetadata("wrapper_type"));
+        $this->assertEquals("data://text/plain,foo", $stream->getMetadata("uri"));
+        $this->assertNull($stream->getMetadata("qux"));
+    }
+    
+    public function testGetMetadataClosed()
+    {
+        $fp = fopen('data://text/plain,foo', 'r');
+        $stream = new Stream($fp);
+        
+        fclose($fp);
+        
+        $this->assertNull($stream->getMetadata());
+        $this->assertNull($stream->getMetadata("uri"));
+    }
+}
